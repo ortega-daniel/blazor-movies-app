290ea9f26860ddf0a33110f4e66a90168a90c405
ortega-daniel <ic.danielortega@gmail.com>
2022-05-06 10:33:54 -0700

    movie dto

--- BlazorApp/Shared/Dtos/MovieDto.cs
+++ BlazorApp/Shared/Dtos/MovieDto.cs
@@ -12,7 +12,28 @@ namespace BlazorApp.Shared.Dtos
         public string Poster { get; set; }
         [Required(ErrorMessage = "this field is required")]
         public DateTime? ReleaseDate { get; set; }
-        public List<GenreDto> Genres { get; set; }
-        public List<ActorDto> Actors { get; set; }
+        public string TitleFit
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Title))
+                {
+                    return null;
+                }
+
+                if (Title.Length > 60)
+                {
+                    return Title.Substring(0, 57) + "...";
+                }
+                else
+                {
+                    return Title;
+                }
+            }
+        }
+        [MinLength(1)]
+        public List<GenreDto> Genres { get; set; } = new();
+        [MinLength(1)]
+        public List<ActorDto> Actors { get; set; } = new();
     }
 }
